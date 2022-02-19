a4278f5d7ea636dccfa2ac4d99805302cab02bca
nut1414 <nut.pannapat@gmail.com>
2022-02-19 23:52:25 +0700

    Fix user upload picture + change error response

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,7 +1,25 @@
 import express from 'express'
+import multer from 'multer'
+import { nanoid } from 'nanoid'
+import * as mime from 'mime-types'
 import Article from '../models/article.js'
 
+
 const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        req.filename = nanoid() + '.' + mime.extension(file.mimetype)
+        cb(null,req.filename)
+    }
+})
+const upload = multer({storage})
+
+
+
 router.get('/', (req, res) => {
     Article.find().then(article_data =>{
         if(article_data){
@@ -9,7 +27,7 @@ router.get('/', (req, res) => {
             console.log(article_data)
         }else throw 'User not found'
     }).catch((err)=>{
-        res.status(200).send({status:404,err})
+        res.status(404).send({error:err})
         console.log(err)
     })
 
@@ -23,5 +41,19 @@ router.get('/:id', async (req, res) => {
     res.status(200).send(article)
 })
 
+
+router.post('/:id/picup', upload.single('image'), (req, res) => {
+    console.log(req.file)
+    Article.findOneAndUpdate({user_id:req.params.id},{}).then(article_data => {
+        if(article_data){
+            res.status(200).send(article_data)
+            console.log(article_data)
+        }else throw 'User not found'
+    }).catch((err)=>{
+        res.status(404).send({error:err})
+        console.log(err)
+    })
+
+})
 
 export default router
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ import express from 'express'
 import multer from 'multer'
 import { nanoid } from 'nanoid'
 import * as mime from 'mime-types'
+import fs from 'fs'
+import path from 'path'
 import User from '../models/user.js'
 
 const router = express.Router()
@@ -10,8 +12,8 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/')
     },
     filename: (req, file, cb) => {
-        let filename = nanoid() + '.' + mime.extension(file.mimetype)
-        cb(null,filename)
+        req.filename = nanoid() + '.' + mime.extension(file.mimetype)
+        cb(null,req.filename)
     }
 })
 const upload = multer({storage})
@@ -24,7 +26,7 @@ router.get('/', (req, res) => {
             console.log(user_data)
         }else throw 'User not found'
     }).catch((err)=>{
-        res.status(200).send({status:404,err})
+        res.status(404).send({error:err})
         console.log(err)
     })
 
@@ -38,25 +40,35 @@ router.get('/:id', (req, res) => {
             console.log(user_data)
         }else throw 'User not found'
     }).catch((err)=>{
-        res.status(200).send({status:404,err})
+        res.status(404).send({error:err})
         console.log(err)
     })
 
 })
 
-router.post('/:id/picup', upload.single('image'), (req, res) => {
-    console.log(req.file)
-    User.findOne({user_id:req.params.id}).then(user_data => {
-        if(user_data){
-            res.status(200).send(user_data)
-            console.log(user_data)
-        }else throw 'User not found'
-    }).catch((err)=>{
-        res.status(200).send({status:404,err})
-        console.log(err)
-    })
-
+router.post('/:id/picup', upload.single('image'), async (req, res) => {
+    let user_data = await User.findOne({user_id:req.params.id})
+    if(req.filename && user_data){
+        await fs.promises.unlink(path.resolve('uploads',user_data.profile_pic)).catch(e=>console.log(e))
+        try{
+            User.findOneAndUpdate({user_id:req.params.id},{profile_pic:req.filename},{new:true}).then(update_ud=>{
+                if(update_ud){
+                    console.log(req.filename)
+                    res.status(200).send(update_ud)
+                    console.log(update_ud)
+                }else throw 'User not found'
+            })
+        }catch(err){
+            res.status(404).send({error:err})
+            console.log(err)
+            User.updateOne({user_id:req.params.id},{profile_pic:'default.jpg'})
+        }
+    }else{
+        res.status(404).send({error:'Unable to complete request.'})
+    }
 })
+
+
 
 
 export default router
